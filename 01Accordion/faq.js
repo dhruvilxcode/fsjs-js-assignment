bf3576a2516ea2cc2e426c6de884cee006543eb4
dhruvilxcode <dhruvilxcode@gmail.com>
2022-09-19 17:21:21 +0530

    1-accordian assignment completed.

--- 01Accordion/faq.js
+++ 01Accordion/faq.js
@@ -18,16 +18,68 @@ const faqData = [
   },
 ];
 
-const accordianBody;
+const accordianBody = document.querySelector(".accordian_body");
 const faqs = [];
 
-function showFaq() {
-  
+
+faqData.forEach((faqD)=>{
+  const faqItemElement = createFaq(faqD.id, faqD.question, faqD.answer);
+  accordianBody.appendChild(faqItemElement)
+});
+
+function createFaq(id, question, answer) {
+  // faq container
+  const faq = document.createElement("div");
+  faq.setAttribute("data-id", id);
+  faq.classList.add("faq");
+
+  // faq text
+  const faqHeaderContainer = document.createElement("div")
+  faqHeaderContainer.classList.add("faq_header");
+  const faqHeader = document.createElement("h3")
+  faqHeader.innerText = question;
+  faqHeaderContainer.appendChild(faqHeader);
+
+  const expandIcon = document.createElement("span");
+  expandIcon.classList.add("material-symbols-outlined");
+  expandIcon.classList.add("show_btn");
+  expandIcon.innerText = "expand_circle_down";
+  expandIcon.addEventListener('click', function(){
+    showFaq(id);
+  });
+
+  faqHeaderContainer.appendChild(expandIcon);
+  faq.appendChild(faqHeaderContainer);
+
+  // faq content
+  const faqContent = document.createElement("p");
+  faqContent.classList.add("hidden");
+  faqContent.innerText = answer;
+  faq.appendChild(faqContent);
+
+  return faq;
 }
 
-function createFaq() {
+function showFaq(id) {
+  const faqContainer = document.querySelector(`div[data-id='${id}']`);
+  const faqAnswer = faqContainer.querySelector("p");
+  const expandIcon = faqContainer.querySelector(".material-symbols-outlined");
   
+  if(faqAnswer.classList.contains("hidden")) {
+    faqAnswer.classList.remove("hidden");
+    faqAnswer.style.display = "block";
+
+    // change the icon
+    expandIcon.innerText = "expand_less";
+  } else {
+    faqAnswer.classList.add("hidden");
+    faqAnswer.style.display = "none";
+
+    // change the icon
+    expandIcon.innerText = "expand_circle_down";
+  }
 }
+
 
 function btnStatusUpdate() {
   
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # fsjs-js-assignment
+
+# 1-Accordian
+
+![1-accordian](./readme_assets/1accordian.gif)
