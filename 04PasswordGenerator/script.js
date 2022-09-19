8aa1e761329a815cf4747a98efcac41a9a22c102
dhruvilxcode <dhruvilxcode@gmail.com>
2022-09-19 21:02:21 +0530

    4-password generator added.

--- 04PasswordGenerator/script.js
+++ 04PasswordGenerator/script.js
@@ -7,6 +7,8 @@ const symbolsEl = document.getElementById('symbols')
 const generateEl = document.getElementById('generate')
 const clipboardEl = document.getElementById('clipboard')
 
+const result = document.getElementById("result");
+
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
@@ -15,25 +17,67 @@ const randomFunc = {
 }
 
 clipboardEl.addEventListener('click', () => {
-    
+    const password = result.innerText;
+
+    // copy to clipboard using navigator api
+    navigator.clipboard.writeText(password);
 })
 
 generateEl.addEventListener('click', () => {
-    
+    const length = lengthEl.value;
+    const isLower = lowercaseEl.checked;
+    const isUpper = uppercaseEl.checked;
+    const isNumber = numbersEl.checked;
+    const isSymbols = symbolsEl.checked;
+
+    const pass = generatePassword(isLower, isUpper, isNumber, isSymbols, length);
+
+    result.innerText = pass;
 })
 
 function generatePassword(lower, upper, number, symbol, length) {
-    
+    let tempPass="";
+    let i = 0;
+    if (length === 0 || (!lower && !upper && !number && !symbol)) {
+        return tempPass;
+    } 
+    while( i < length ) {
+        if(lower) {
+            tempPass += randomFunc.lower();
+            i++;
+        }
+        if(upper) {
+            tempPass += randomFunc.upper();
+            i++;
+        }
+        if(number) {
+            tempPass += randomFunc.number();
+            i++;
+        }
+        if(symbol) {
+            tempPass += randomFunc.symbol();
+            i++;
+        }
+    }
+    return tempPass;
 }
 
 function getRandomLower() {
+    const lower = "abcdefghijklmnopqrstuvwxyz";
+    return lower[Math.floor(Math.random() * lower.length)];
 }
 
 function getRandomUpper() {
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return upper[Math.floor(Math.random() * upper.length)];
 }
 
 function getRandomNumber() {
+    const numbers = "0123456789";
+    return numbers[Math.floor(Math.random() * numbers.length)];
 }
 
 function getRandomSymbol() {
+    const symbols = "-+,.;'[]=()*#@!";
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # fsjs-js-assignment
 
 # 1-Accordian
-
 ![1-accordian](./readme_assets/1accordian.gif)
 
 # 2-Name-Conversion
@@ -9,3 +8,7 @@
 
 # 3-Color-Changer
 ![3-Color-change](./readme_assets/3-color-changer.gif)
+
+# 4-Password-Generator
+![4-password-generator](./readme_assets/4-password-generator.gif)
+
